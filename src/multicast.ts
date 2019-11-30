2c6547e52710aba062c02c88c1979e5cb55feb58
Andy Wermke <github@andywer.com>
2019-11-30 17:35:45 +0100

    Let multicast() subscribe lazily and unsubscribe (#2)

--- src/multicast.ts
+++ src/multicast.ts
@@ -1,3 +1,4 @@
+import { unsubscribe } from "./_util"
 import Observable, { ObservableLike } from "./observable"
 import Subject from "./subject"
 
@@ -15,11 +16,38 @@ import Subject from "./subject"
  *
  * A hot observable is an observable where new subscribers subscribe to
  * the upcoming values of an already-initialiazed observable.
+ *
+ * The multicast observable will lazily subscribe to the source observable
+ * once it has its first own subscriber and will unsubscribe from the
+ * source observable when its last own subscriber unsubscribed.
  */
 function multicast<T>(coldObservable: ObservableLike<T>): Observable<T> {
-  const proxy = new Subject<T>()
-  coldObservable.subscribe(proxy)
-  return Observable.from(proxy)
+  const subject = new Subject<T>()
+
+  let sourceSubscription: ReturnType<ObservableLike<T>["subscribe"]> | undefined
+  let subscriberCount = 0
+
+  return new Observable<T>(observer => {
+    // Init source subscription lazily
+    if (!sourceSubscription) {
+      sourceSubscription = coldObservable.subscribe(subject)
+    }
+
+    // Pipe all events from `subject` into this observable
+    const subscription = subject.subscribe(observer)
+    subscriberCount++
+
+    return () => {
+      subscriberCount--
+      subscription.unsubscribe()
+
+      // Close source subscription once last subscriber has unsubscribed
+      if (subscriberCount === 0) {
+        unsubscribe(sourceSubscription)
+        sourceSubscription = undefined
+      }
+    }
+  })
 }
 
 export default multicast
